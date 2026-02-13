0ed1c5b0271167b65d55492f5118bb784a9b47d2
PSahilKumar <p.sahilkumar2005@gmail.com>
2026-02-13 23:05:15 +0530

    Made some changes in portfolio

--- script.js
+++ script.js
@@ -12,26 +12,47 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+    // sections.forEach(sec => {
+    //     let top = window.scrollY;
+    //     let offset = sec.offsetTop - 100;
+    //     let height = sec.offsetHeight;
+    //     let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            // active navbar links
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-            // active sections for animation on scroll
-            sec.classList.add('show-animate');
-        }
-        // if want to animation that repeats on scroll use this
-        else {
-            sec.classList.remove('show-animate');
-        }
-    });
+    //     if(top >= offset && top < offset + height) {
+    //         // active navbar links
+    //         navLinks.forEach(links => {
+    //             links.classList.remove('active');
+    //             document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+    //         });
+    //         // active sections for animation on scroll
+    //         sec.classList.add('show-animate');
+    //     }
+    //     // if want to animation that repeats on scroll use this
+    //     else {
+    //         sec.classList.remove('show-animate');
+    //     }
+    // });
+    sections.forEach(sec => {
+
+    let rect = sec.getBoundingClientRect();
+    let id = sec.getAttribute('id');
+
+    // Section is considered active when its top reaches 60% of screen
+    if (rect.top <= window.innerHeight * 0.6 && rect.bottom >= 100) {
+
+        // active navbar links
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+
+        document.querySelector('header nav a[href*=' + id + ']')
+            .classList.add('active');
+
+        sec.classList.add('show-animate');
+
+    }
+});
+
 
     // sticky navbar
     let header = document.querySelector('header');
